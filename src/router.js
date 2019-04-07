ceb6d817714c3584422f33f163264f236c846b82
Koichi Taura <taura.koichi@gmail.com>
2019-04-07 18:11:55 +0900

    add not found page

--- src/router.js
+++ src/router.js
@@ -6,6 +6,7 @@ import Works from './views/Works.vue'
 import Contact from './views/Contact.vue'
 import Blog from './views/Blog.vue'
 import Detail from './views/Detail.vue'
+import NotFound from './views/NotFound.vue'
 
 Vue.use(Router)
 
@@ -43,13 +44,9 @@ export default new Router({
             component: Detail
         },
         {
-            path: '/about',
-            name: 'about',
-            // route level code-splitting
-            // this generates a separate chunk (about.[hash].js) for this route
-            // which is lazy-loaded when the route is visited.
-            component: () =>
-                import ( /* webpackChunkName: "about" */ './views/About.vue')
+            path: '*',
+            name: 'error',
+            component: NotFound
         }
     ]
 })
---
+++ src/views/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="main container">
+    <div class="row">
+      <div class="error-message main-contents col-lg-8 col-md-10 mx-auto">
+        <h1>ERROR - Not Found</h1>
+        <h2 class="subheading">お探しのページが見つかりませんでした。</h2>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'notfound'
+}
+</script>
