bf0d2ce5071ce2da22f3473df40dc1894cb24378
Koichi Taura <taura.koichi@gmail.com>
2019-04-13 17:47:52 +0900

    modify booklog shelf

---
+++ src/components/Booklog.vue
@@ -0,0 +1,36 @@
+<template>
+<div v-html="this.widgetScript">
+</div>
+</template>
+<script>
+import axios from 'axios';
+axios.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+export default {
+  name: 'Booklog',
+  data() {
+      return {
+          widgetScript: '<script id="booklog_minishelf" type="text/javascript" />',
+      }
+  },
+  head() {
+    return {
+      script: [
+        {src: '/js/booklog.js'},
+      ]
+    }
+  },
+  created() {
+
+  },
+  mounted: function() {
+    this.readBooklogApi();
+  },
+  methods: {
+      readBooklogApi() {
+        let booklogScript = document.createElement('script')
+        booklogScript.setAttribute('src', 'https://api.booklog.jp/json/taurin190?category=0&count=50&callback=booklog')
+        document.body.appendChild(booklogScript)
+      }
+  } 
+}
+</script>
--- src/components/SocialActivity.vue
+++ src/components/SocialActivity.vue
@@ -5,31 +5,31 @@
     <div class="col-lg-12 col-md-12">
       <h3 class="subheading">Github</h3>
       <a href="http://github.com/Taurin190" class="gh-contributions" data-gh-username="Taurin190" target="_blank">Taurin190's contributions</a>
-      <!-- <script src="https://ikuwow.github.io/yourcontributions/widget.min.js"></script> -->
     </div>
     <div class="col-lg-6 col-md-6 rounded float-left">
       <h3 class="subheading">Twitter</h3>
       <a class="twitter-timeline" data-width="600" data-height="800" href="https://twitter.com/Tauitter51?ref_src=twsrc%5Etfw">Tweets by Tauitter51</a>
-      <!-- <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> -->
     </div>
     <div class="col-lg-6 col-md-6 rounded float-right">
       <h3 class="subheading">Booklog</h3>
-      <!-- <script type="text/javascript" src="http://widget.booklog.jp/blogparts/js/booklog_minishelf.js?default" id="booklog_minishelf"></script>
-      <script type="text/javascript" src="http://api.booklog.jp/json/taurin190?category=0&count=40&callback=booklog_minishelf"></script> -->
+      <Booklog />
     </div>
   </div>
 </div>
 </template>
 <script>
+import Booklog from '@/components/Booklog.vue'
+
 export default {
   name: 'SocialActivity',
+  components: {
+    Booklog
+  },
   head() {
     return {
       script: [
         {src: 'https://ikuwow.github.io/yourcontributions/widget.min.js'},
         {src: 'https://platform.twitter.com/widgets.js', async: true},
-        {src: 'http://widget.booklog.jp/blogparts/js/booklog_minishelf.js?default'},
-        {src: 'http://api.booklog.jp/json/taurin190?category=0&count=40&callback=booklog_minishelf'}
       ]
     }
   }
